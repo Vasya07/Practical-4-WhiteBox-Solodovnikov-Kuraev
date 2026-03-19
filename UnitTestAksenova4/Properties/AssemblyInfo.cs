e3d08fa2481596a328b9bdabb0c0134e957b6dc1
Vasya <quartz880@yandex.ru>
2026-03-19 13:49:12 +0300

    Добавлен проект с unt-тестами , а также написан простой успешный unit-тест (проект : "UnitTestAksenova")

---
+++ UnitTestAksenova4/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestAksenova4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestAksenova4")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aaf05221-7fe7-4d07-8ba8-ed28c4d51a63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
